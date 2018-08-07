e5a9875519817645c332dd3bcc072a17c1370985
Cochrane, Riley O'Neill <riley.cochrane@me.com>
2018-08-07 12:50:16 +1200

    Upto Django project tutorial Part 4

--- AdminView/models.py
+++ AdminView/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class LabSlot(models.Model):
+    lab_time = models.TimeField('Time Chosen')
+    lab_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.lab_name + " @ " + self.lab_time.strftime('%H:%M:%S')
+
+
+class LabDemos(models.Model):
+    lab = models.ForeignKey(LabSlot, on_delete=models.CASCADE)
+    chosen_demos = models.CharField(max_length=200)
--- AdminView/views.py
+++ AdminView/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.template import loader
 # Create your views here.
+from .models import LabSlot
+
+
+def index(request):
+    lab_list = LabSlot.objects.order_by('-lab_name')[:5]
+    template = loader.get_template('AdminView/index.html')
+    context = {
+        'lab_list': lab_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, labslot_id):
+    lab = get_object_or_404(LabSlot, pk=labslot_id)
+    return render(request, 'AdminView/detail.html', {'lab': lab})
